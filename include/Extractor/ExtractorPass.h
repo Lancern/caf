dc133bc43f0bd927c7e77ebc1604799b17ef88ae
Lancern <msrlancern@126.com>
2020-02-12 10:01:33 +0000

    Expose ExtractorPass class in a header file.

---
+++ include/Extractor/ExtractorPass.h
@@ -0,0 +1,44 @@
+#ifndef CAF_EXTRACTOR_PASS_H
+#define CAF_EXTRACTOR_PASS_H
+
+#include "Extractor/ExtractorContext.h"
+
+#include "llvm/Pass.h"
+
+namespace caf {
+
+/**
+ * @brief CAF extractor analysis pass.
+ *
+ */
+class ExtractorPass : public llvm::ModulePass {
+public:
+  /**
+   * @brief Construct a new ExtractorPass object.
+   *
+   */
+  explicit ExtractorPass();
+
+  /**
+   * @brief Get the context of the extractor pass.
+   *
+   * @return ExtractorContext& context of the extractor pass.
+   */
+  ExtractorContext& context() { return _context; }
+
+  /**
+   * @brief Get the context of the extractor pass.
+   *
+   * @return const ExtractorContext& context of the extractor pass.
+   */
+  const ExtractorContext& context() const { return _context; }
+
+  bool runOnModule(llvm::Module& module) override;
+
+private:
+  ExtractorContext _context;
+}; // class ExtractorPass
+
+} // namespace caf
+
+#endif
--- src/Extractor/ExtractorPass.cpp
+++ src/Extractor/ExtractorPass.cpp
@@ -1,6 +1,6 @@
 #include "Basic/CAFStore.h"
 #include "Basic/JsonSerializer.h"
-#include "Extractor/ExtractorContext.h"
+#include "Extractor/ExtractorPass.h"
 #include "ExtractorUtils.h"
 
 #include "llvm/Pass.h"
@@ -15,6 +15,8 @@
 #include <utility>
 #include <string>
 
+namespace caf {
+
 namespace {
 
 llvm::cl::opt<std::string> CAFStoreOutputFileName(
@@ -22,82 +24,54 @@ llvm::cl::opt<std::string> CAFStoreOutputFileName(
     llvm::cl::desc("Specify the path to which the CAF store data will be serialized."),
     llvm::cl::value_desc("path"));
 
-/**
- * @brief CAF extractor analysis pass.
- *
- */
-class ExtractorPass : public llvm::ModulePass {
-public:
-  /**
-   * @brief Construct a new ExtractorPass object.
-   *
-   */
-  explicit ExtractorPass()
-    : llvm::ModulePass { ID }
-  { }
-
-  /**
-   * @brief Get the extractor context.
-   *
-   * @return caf::ExtractorContext& the extractor context.
-   */
-  caf::ExtractorContext& context() { return _context; }
-
-  /**
-   * @brief Get the extractor context.
-   *
-   * @return caf::ExtractorContext& the extractor context.
-   */
-  const caf::ExtractorContext& context() const { return _context; }
-
-  bool runOnModule(llvm::Module &module) override {
-    for (auto& func : module) {
-      if (caf::IsApiFunction(func)) {
-        _context.AddApiFunction(&func);
-      } else if (caf::IsConstructor(func)) {
-        auto name = caf::GetConstructingTypeName(func);
-        _context.AddConstructor(std::move(name), &func);
-      }
-
-      _context.AddCallbackFunctionCandidate(&func);
-    }
-
-    _context.Freeze();
-
-    auto outputFileName = CAFStoreOutputFileName.c_str();
-    if (outputFileName && strlen(outputFileName)) {
-      do {
-        std::error_code openFileError { };
-        llvm::raw_fd_ostream outputFile {outputFileName, openFileError };
-        if (openFileError) {
-          llvm::errs() << "CAFDriver: failed to dump metadata to file: "
-              << openFileError.value() << ": "
-              << openFileError.message() << "\n";
-          break;
-        }
-
-        auto store = _context.CreateCAFStore();
-        caf::JsonSerializer jsonSerializer;
-        auto json = jsonSerializer.Serialize(*store);
-        outputFile << json.dump();
-        outputFile.flush();
-        outputFile.close();
-
-        llvm::errs() << "CAF store data has been saved to " << outputFileName << "\n";
-      } while (false);
-    } else {
-      llvm::errs() << "CAF store data will not be persisted since -cafstore is not provided.\n";
-    }
-
-    return false;
-  }
-
-private:
-  caf::ExtractorContext _context;
-
-  static char ID;
-}; // class ExtractorPass
-
-char ExtractorPass::ID = 0;
+char PassId;
 
 } // namespace <anonymous>
+
+ExtractorPass::ExtractorPass()
+  : llvm::ModulePass { PassId }
+{ }
+
+bool ExtractorPass::runOnModule(llvm::Module &module) {
+  for (auto& func : module) {
+    if (caf::IsApiFunction(func)) {
+      _context.AddApiFunction(&func);
+    } else if (caf::IsConstructor(func)) {
+      auto name = caf::GetConstructingTypeName(func);
+      _context.AddConstructor(std::move(name), &func);
+    }
+
+    _context.AddCallbackFunctionCandidate(&func);
+  }
+
+  _context.Freeze();
+
+  auto outputFileName = CAFStoreOutputFileName.c_str();
+  if (outputFileName && strlen(outputFileName)) {
+    do {
+      std::error_code openFileError { };
+      llvm::raw_fd_ostream outputFile {outputFileName, openFileError };
+      if (openFileError) {
+        llvm::errs() << "CAFDriver: failed to dump metadata to file: "
+            << openFileError.value() << ": "
+            << openFileError.message() << "\n";
+        break;
+      }
+
+      auto store = _context.CreateCAFStore();
+      caf::JsonSerializer jsonSerializer;
+      auto json = jsonSerializer.Serialize(*store);
+      outputFile << json.dump();
+      outputFile.flush();
+      outputFile.close();
+
+      llvm::errs() << "CAF store data has been saved to " << outputFileName << "\n";
+    } while (false);
+  } else {
+    llvm::errs() << "CAF store data will not be persisted since -cafstore is not provided.\n";
+  }
+
+  return false;
+}
+
+} // namespace caf

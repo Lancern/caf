2521dc669e95443145a813132b23fb82d85d775a
Lancern <msrlancern@126.com>
2020-03-28 04:24:19 +0000

    Fix build issues.

--- src/Targets/v8/V8Target.cpp
+++ src/Targets/v8/V8Target.cpp
@@ -1,14 +1,16 @@
 #include "Targets/V8/V8Target.h"
 #include "Targets/V8/V8Traits.h"
 
+#define API_FUNCS callbackfunc_candidates
+
 extern "C" {
-  extern void** ApiFunctions;
+  extern void** API_FUNCS;
 }
 
 namespace caf {
 
 typename V8Traits::ApiFunctionPtrType GetApiFunction(uint32_t funcId) {
-  return reinterpret_cast<typename V8Traits::ApiFunctionPtrType>(ApiFunctions);
+  return reinterpret_cast<typename V8Traits::ApiFunctionPtrType>(API_FUNCS[funcId]);
 }
 
 }
